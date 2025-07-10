833c8829338f14b57067451677519a7175306baf
Willian Zamora <wzamora@gmail.com>
2025-07-10 09:54:01 -0500

    Update energy_meter.py
    Error selection month is not numeric

--- models/energy_meter.py
+++ models/energy_meter.py
@@ -252,10 +252,10 @@ class MeterConsumption(models.Model):
     # Date fields
     year = fields.Integer(string='Year', required=True)
     month = fields.Selection([
-        (1, 'January'), (2, 'February'), (3, 'March'),
-        (4, 'April'), (5, 'May'), (6, 'June'),
-        (7, 'July'), (8, 'August'), (9, 'September'),
-        (10, 'October'), (11, 'November'), (12, 'December')
+        ('1', 'January'), ('2', 'February'), ('3', 'March'),
+        ('4', 'April'), ('5', 'May'), ('6', 'June'),
+        ('7', 'July'), ('8', 'August'), ('9', 'September'),
+        ('10', 'October'), ('11', 'November'), ('12', 'December')
     ], string='Month', required=True)
     
     year_month = fields.Char(
