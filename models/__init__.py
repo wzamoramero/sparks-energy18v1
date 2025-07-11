b277b66e8893b63075149f8a726b92084ef3f88f
Willian Zamora <wzamora@gmail.com>
2025-07-11 10:36:41 -0500

    Update __init__.py
    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from . import consumption_line
-from . import solar_quotation
 from . import auxiliary_models
 from . import energy_meter
 from . import solar_panel_product
+from . import solar_quotation
 from . import res_partner
